2219436f17a3e7bbb44ff6e18eed328a62314210
muhammad nur rahman <99724372+sarrahman@users.noreply.github.com>
2022-09-03 09:10:47 +0600

    Update index.jsx

--- src/containers/pages/Harga/index.jsx
+++ src/containers/pages/Harga/index.jsx
@@ -1,4 +1,4 @@
-import { Alert, AlertTitle, Box } from "@mui/material";
+import { Box } from "@mui/material";
 import AppBarComp from "../../../components/AppBar";
 import TableComp from "../../../components/Table";
 import { connect } from "react-redux";
@@ -21,13 +21,6 @@ const Harga = (props) => {
   return (
     <div>
       <AppBarComp title="kerajaan keramik" />
-      <Alert severity="warning" sx={{
-        mt: 2,
-        mx: 2
-      }}>
-        <AlertTitle>Warning !</AlertTitle>
-        Mulai 28 November 2022, Situs ini akan ditutup dikarenakan server tidak tersedia lagi,  Anda harus meningkatkan versi ke paket berbayar sebelum tanggal ini untuk memastikan aplikasi Anda terus berjalan dan untuk mempertahankan data Anda — <strong>Sarrahman Group</strong>
-      </Alert>
       <Box
         sx={{
           display: "flex",
