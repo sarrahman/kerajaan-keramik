e87928982bf7778ba9d927064c28f30efe6d7c59
muhammad nur rahman <99724372+sarrahman@users.noreply.github.com>
2022-12-01 05:40:35 +0800

    Update harga.jsx

--- src/containers/pages/Harga/admin/harga.jsx
+++ src/containers/pages/Harga/admin/harga.jsx
@@ -22,12 +22,6 @@ const HargaAdmin = (props) => {
   return (
     <div>
       <AppBarComp title="kerajaan keramik" />
-//     <Alert sx={{
-//     m: 2
-//     }} severity="warning">
-//   <AlertTitle>Warning</AlertTitle>
-//   Reminder Upgrade now! Heroku free product plans end November 28th — <strong>salesforce team</strong>
-// </Alert>
       <Box
         sx={{
           display: "flex",
